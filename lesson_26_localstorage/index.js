249b1faf7e85863ac84b7d160c55bf8ce9f805a5
Swimi12 <bkravets0@gmail.com>
2022-11-07 13:10:24 +0300

    add homework local storage

--- lesson_26_localstorage/index.js
+++ lesson_26_localstorage/index.js
@@ -1,31 +1,34 @@
-localStorage.setItem("test", 34234);
-localStorage.getItem("test");
-console.log('localStorage.getItem("test");: ', localStorage.getItem("test"));
-
-const user = {
-  name: "Oleg",
-};
-const userStringufy = JSON.stringify(user);
-localStorage.setItem("user", userStringufy);
-
-localStorage.removeItem("test");
+let list;
+let keyStringify;
 
 class LocalStorage {
-  key = "Default key";
-  value = "Defaulet value";
+  key = 'Default key';
+  value = 'Defaulet value';
   constructor(key, value) {
     if (key && value) {
-      localStorage.setItem((this.key = key), (this.value = value));
+      this.key = key;
+      this.value = value;
+      list = [value];
+      keyStringify = JSON.stringify(list);
+      localStorage.setItem(key, keyStringify);
     } else {
-      throw new Error("Enter key and value");
+      throw new Error('Enter key and value');
     }
   }
+  addItem = () => {
+    const oldStorageList = localStorage.getItem(this.key);
+    const list = JSON.parse(oldStorageList);
+    const newItem = prompt();
+    list.push(newItem);
+    localStorage.setItem(this.key, JSON.stringify(list));
+  };
 
   static clearLocarStorage(key) {
     localStorage.removeItem(key);
   }
 }
-const test1 = new LocalStorage("admin", "123");
-const test2 = new LocalStorage("adminЄ", "1234");
-const test3 = new LocalStorage("admin12312321Є", 1234);
-LocalStorage.clearLocarStorage("adminЄ");
+const test1 = new LocalStorage('admin', '123');
+test1.addItem();
+// const test2 = new LocalStorage('admin', '1234');
+// const test3 = new LocalStorage('admin12312321Є', 1234);
+// LocalStorage.clearLocarStorage('adminЄ');
